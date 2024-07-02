c24371e8c568b9c4c8f19096abbc62c423b82468
cannorin <cannorin@users.noreply.github.com>
2024-07-02 16:57:26 +0900

    Disable ProduceReferenceAssembly

--- tests/Repro.Tests.fs
+++ tests/Repro.Tests.fs
@@ -3,18 +3,20 @@ module ReproTests
 open Repro
 open NUnit.Framework
 
+let areEqual (a: 'a, b) = Assert.That(a, Is.EqualTo(b))
+
 [<Test>]
 let ``Default constructor should create instance`` () =
-    Assert.AreEqual("My internal state", MyType().InnerState)
+    areEqual ("My internal state", MyType().InnerState)
 
 [<Test>]
 let ``Constructor with parameter should create instance`` () =
-    Assert.AreEqual("override", MyType("override").InnerState)
+    areEqual ("override", MyType("override").InnerState)
 
 [<Test>]
 let ``Method with ReflectedDefinition parameter should get its name`` () =
     let myValue = 2
-    Assert.AreEqual("myValue", MyType.NameOf(myValue))
+    areEqual ("myValue", MyType.NameOf(myValue))
 
 type Generative2 = Repro.GenerativeProvider<2>
 type Generative4 = Repro.GenerativeProvider<4>
@@ -22,14 +24,13 @@ type Generative4 = Repro.GenerativeProvider<4>
 [<Test>]
 let ``Can access properties of generative provider 2`` () =
     let obj = Generative2()
-    Assert.AreEqual(obj.Property1, 1)
-    Assert.AreEqual(obj.Property2, 2)
+    areEqual (obj.Property1, 1)
+    areEqual (obj.Property2, 2)
 
 [<Test>]
 let ``Can access properties of generative provider 4`` () =
     let obj = Generative4()
-    Assert.AreEqual(obj.Property1, 1)
-    Assert.AreEqual(obj.Property2, 2)
-    Assert.AreEqual(obj.Property3, 3)
-    Assert.AreEqual(obj.Property4, 4)
-
+    areEqual (obj.Property1, 1)
+    areEqual (obj.Property2, 2)
+    areEqual (obj.Property3, 3)
+    areEqual (obj.Property4, 4)
